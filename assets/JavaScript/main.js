f53fef85a94b903da0969211925800ff150e152a
EduardaBezerra <eduardaf.bezerra@outlook.com>
2022-06-22 13:17:28 -0300

    Menu

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -2,3 +2,10 @@ window.addEventListener('scroll', function(){
     const header = document.querySelector('header');
     header.classList.toggle("sticky", window.scrollY > 0)
 });
+
+function menuToggle() {
+    const menuToggle = document.querySelector('.menuToggle');
+    menuToggle.classList.toogle('active');
+}
+
+onclick = toggleMenu()
